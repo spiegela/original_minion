25d0c5cfcd2058ea25916deb16e1ba27b8c71f9d
Aaron Spiegel <spiegela@gmail.com>
2011-08-21 21:49:58 -0500

    Convert Tabs to Spaces

--- spec/base.rb
+++ spec/base.rb
@@ -5,17 +5,17 @@ require 'mocha/standalone'
 require 'mocha/object'
 
 class Bacon::Context
-	include Mocha::API
+  include Mocha::API
 
-	def initialize(name, &block)
-		@name = name
-		@before, @after = [
-			[lambda { mocha_setup }],
-			[lambda { mocha_verify ; mocha_teardown }]
-		]
-		@block = block
-	end
+  def initialize(name, &block)
+    @name = name
+    @before, @after = [
+      [lambda { mocha_setup }],
+      [lambda { mocha_verify ; mocha_teardown }]
+    ]
+    @block = block
+  end
 
-	def xit(desc, &bk)
-	end
+  def xit(desc, &bk)
+  end
 end
